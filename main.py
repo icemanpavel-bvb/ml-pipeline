ee47960f37d14e0f16bbc6530fc38455bcaae63c
icemanpavel-bvb <iceman.pavel@gmail.com>
2026-03-03 19:53:55 +0500

    fix main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def calculate_average(numbers):
 """Функция вычисляет среднее арифметическое списка чисел."""
 	if len(numbers) == 0:
 	return None
-total = sum(numbers)
-average = total / len(numbers)
-# Ошибка: функция должна возвращать значение
-print(f"Среднее арифметическое: {average}")
+    total = sum(numbers)
+    average = total / len(numbers)
+    # Ошибка: функция должна возвращать значение
+    return average
